a08cc6d1428dc5e2d596b5c9e32fee2b7d536f8f
Mateus Ribeiro <mateus.ribe.sva@gmail.com>
2024-09-24 19:28:18 -0300

    include coord unit tests

---
+++ tests/domain/test_coord.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.domain.coord import Coord
+
+
+def test_valid_coord():
+    lat = -20.23
+    long = -20.23
+
+    coord = Coord(lat, long)
+
+    assert coord.get_long() == long
+    assert coord.get_lat() == lat
+
+
+@pytest.mark.parametrize(
+    'lat, long, message',
+    [
+        (-100, -90, 'invalid latitude'),
+        (-80, 190, 'invalid longitude'),
+        (100, -90, 'invalid latitude'),
+        (80, -190, 'invalid longitude'),
+    ],
+)
+def test_invalid_coord(lat, long, message):
+    with pytest.raises(Exception, match=message):
+        Coord(lat, long)
